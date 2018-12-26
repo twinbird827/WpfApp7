9986a9afab4ffb68eb839ec7bc4a899b2fa1b1ed
twinbird827 <twinbird827@gmail.com>
2018-12-26 17:48:15 +0900

    PasswordBoxの確認用

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            PropertyChanged += (sender, e) => { };
+            //Password = "";
+        }
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; PropertyChanged(this, new PropertyChangedEventArgs("Password")); }
+        }
+        private string _Password = "";
+
+        public string Password2
+        {
+            get { return _Password2; }
+            set { _Password2 = value; PropertyChanged(this, new PropertyChangedEventArgs("Password2")); }
+        }
+        private string _Password2 = "";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new MainWindow();
+            win.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Password = null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+WpfApp7
+====
+
+## Overview
+
+PasswordBox.Passwordをバインディングするためのビヘイビア確認用
+
+## Description
+
+## Demo
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+## Install
+
+## Contribution
+
+## Licence
+
+[MIT](https://github.com/twinbird827/WpfApp7/blob/master/LICENSE)
+
+## Author
+
+[twinbird827](https://github.com/twinbird827)
